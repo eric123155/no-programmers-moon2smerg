24feb5fbc28187d6fbf0dd2337d7616adb5ad276
Eric <lepe.eric@yahoo.com>
2020-05-29 17:33:14 -0700

    Simple AI follow

---
+++ Assets/Scripts/ActorScripts/AI.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+[RequireComponent(typeof(NavMeshAgent))]
+public abstract class AI : MonoBehaviour
+{
+    protected Actor _actor = null;
+    protected NavMeshAgent _agent = null;
+
+    private void Awake() 
+    {
+        _actor = GetComponent<Actor>();
+        _agent = GetComponent<NavMeshAgent>();
+    }
+}
